45ee8c31bde53747d0350a2ed21d379b32a98941
Miguel Anthony <164796190+migss69@users.noreply.github.com>
2026-06-16 11:50:04 -0300

    Add files via upload

---
+++ filme.py
@@ -0,0 +1,15 @@
+from . import db
+from .base import ModeloBase
+
+
+class Filme(ModeloBase):
+    __tablename__ = "filmes"
+
+    titulo = db.Column(db.String(150), nullable=False)
+    duracao_min = db.Column(db.Integer, nullable=False)
+    classificacao = db.Column(db.String(5), nullable=False)
+    sessoes = db.relationship("Sessao", backref="filme", lazy=True)
+    
+    @classmethod
+    def listar(cls):
+        return cls.query.order_by(cls.titulo).all()
---
+++ sala.py
@@ -0,0 +1,13 @@
+from . import db
+from .base import ModeloBase
+
+
+class Sala(ModeloBase):
+    __tablename__ = "salas"
+
+    numero = db.Column(db.Integer, nullable=False)
+    capacidade = db.Column(db.Integer, nullable=False)
+    sessaos = db.relationship("Sessao", back_populates = "sala")
+    @classmethod
+    def listar(cls):
+        return cls.query.order_by(cls.numero).all()
